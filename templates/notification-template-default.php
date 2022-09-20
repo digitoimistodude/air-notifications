6e9d35a886ac0eb8450ba02f66cc81f2c5e9312f
Tuomas Marttila <77620588+Tumppex@users.noreply.github.com>
2022-09-20 10:03:43 +0300

    Modify base template

--- templates/notification-template-default.php
+++ templates/notification-template-default.php
@@ -1,9 +1,9 @@
 <div class="air-notification" data-notification-id="notification-<?php echo esc_html( $notification['guid'] ) ?>">
   <div class="container">
     <?php include plugin_dir_path( __FILE__ ) . '../assets/info-icon.svg'; ?>
-    <p>
+    <div>
       <?php echo wp_kses_post( wpautop( $notification['content'] ) ) ?>
-    </p>
+    </div>
 
     <?php if ( $notification['is_dismissable'] ) : ?>
       <button type="button" class="notification-close air-notification-close" data-notification-id="notification-<?php echo esc_html( $notification['guid'] ) ?>">
